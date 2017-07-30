37b960c21c21b53ff25057a993737ef8535a8bce
Wahid Shafique <wahidshafique@hotmail.com>
2017-07-30 17:25:19 -0400

    .

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { CirclePicker as ColorPicker} from 'react-color';
+import { CirclePicker as ColorPicker, SketchPicker as HelperPicker } from 'react-color';
 import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
@@ -9,7 +9,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      colors: '#a00000',
+      colorsArray: [],
+      color: '#a00000',
       dbRefApp: firebase.database().ref().child('app'),
       dbRefArduino: firebase.database().ref().child('arduino'),
       isArduinoOn: false,
@@ -28,29 +29,40 @@ class App extends Component {
         this.setState({ isArduinoInit: false })
       }
     });
+
+    this.state.dbRefApp.on('value', snap => {
+      if (snap.val() !== null) {
+        this.setState({ color: snap.val().color })
+      }
+    })
   }
 
   handleChangeComplete = (color) => {
-    this.setState({ colors: color.hex });
+    this.setState({ color: color.hex });
     this.state.dbRefApp.set({
-      color: this.state.colors
+      color: this.state.color
     })
   };
 
   render() {
     const colorBoxStyle = {
-      backgroundColor: this.state.colors
+      backgroundColor: this.state.color
     };
 
     return (
       <div className="App">
         <div className="App-header" style={colorBoxStyle}>
           <img src={logo} className="App-logo" alt="logo" />
-          <h3>{this.state.colors}</h3>
+          <h3>{this.state.color}</h3>
           <h3>The Arduino {this.state.isArduinoOn ? 'is on' : 'is off'} {!this.state.isArduinoInit ? "and it's firebase data has not been initialized (try running it for the first time)" : ''}</h3>
+          <h1>test</h1>
         </div>
         <div className="box">
-          <ColorPicker circleSize='55' color={this.state.colors} onChangeComplete={this.handleChangeComplete} />
+          <ColorPicker circleSize='55' color={this.state.colors} onChangeComplete={this.handleChangeComplete}
+          colors={
+            ["#ff0000", "#00ff00","#000fff", "#ffff00", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722", "#795548", "#607d8b"]
+          }
+          />
         </div>
 
       </div>
